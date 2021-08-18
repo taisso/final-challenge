8010c920d7961345d42bec3203e36163e61dfc36
taisso <taissoreni@gmail.com>
2021-08-18 14:02:54 -0300

    refactor: styles when selecting balls with JS

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,36 @@
 (function (win, doc) {
   "use strict";
 
+  const $balls = doc.querySelectorAll(".balls span");
+  const $buttonsGame = doc.querySelectorAll(".tags button");
+
   function app() {
     return {
       run() {
+        this.initEvents();
       },
+
+      initEvents() {
+        Array.prototype.forEach.call($balls, (ball) => {
+          ball.addEventListener("click", this.handleSelectBalls);
+        });
+      },
+
+      handleSelectBalls() {
+        app().ballsStyle.call(this);
+      },
+
+      ballsStyle() {
+        const styles = {
+          backgroundColor: "#fff",
+          color: "#adc0c4",
+          border: "1px solid #adc0c4",
+        };
+
+        Object.keys(styles).forEach((key) => (this.style[key] = styles[key]));
+      },
+    };
   }
 
   app().run();
 })(window, document);
-
